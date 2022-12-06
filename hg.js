f40b1d1ca1786c8c8b3267ac0e6561c8e07f3c57
rulink10 <rmunguia16@outlook.com>
2022-12-06 08:45:22 -0600

    Agregando JSON incompleto

--- hg.js
+++ hg.js
@@ -17,49 +17,8 @@ for (let tarjeta of tarjetas){
     tarjeta.innerHTML = tarjeta.innerHTML;
 }
 
-
-
-
-class Producto {
-  constructor(name, sku, price, inventory) {
-    this.name = name;
-    this.sku = sku;
-    this.price = price;
-    this.inventory = inventory;
-  }
-}
-
-const productos = [];
-
-productos.push(new Producto("Nintendo Switch", "000001", 5999.99, 50));
-productos.push(new Producto("Playstation 5", "000002", 15999.99, 5));
-productos.push(new Producto("Amiibo Zelda & Loftwing", "000003", 899.99, 50));
-productos.push(new Producto("Amiibo Minecraft", "000004", 899.99, 50));
-productos.push(new Producto("Figma Princesa Zelda Twillight Princess ","000005",3999.99,10)
-);
-
-class Carrito {
-    constructor(sku, cantidad, precio) {
-        this.sku = sku;
-        this.cantidad = cantidad;
-        this.precio = precio;
-    }
-}
-
 let img,i = 0,carrito = [],total = 0,articulos;
 
-/*for (let producto of productos) {
-    tarjeta[i].getElementsByTagName("h3")[0].innerText = producto.name;
-    tarjeta[i].getElementsByClassName("precio")[0].innerText =
-        "$" + producto.price;
-    tarjeta[i].getElementsByClassName("cantidad")[0].innerText =
-        "Disponible(s): " + producto.inventory;
-    img = "./assets/" + producto.sku + ".jpg";
-    tarjeta[i].getElementsByTagName("img")[0].src = img;
-    i++;
-    console.log(tarjeta[i]);
-    carrito.push(new Carrito(producto.sku, 0, 0.0));
-}*/
 
 function agregarAlCarrito(e) {
   console.log("Evento Agregado");
